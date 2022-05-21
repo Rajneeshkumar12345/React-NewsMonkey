3c81c7116159583fa3217b5eff8cd9c55b737fbd
Rajneeshkumar12345 <rkv7861999@gmail.com>
2022-05-21 23:09:47 +0530

    news

---
+++ src/components/NewsItem.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+
+export class NewsItem extends Component {
+  render() {
+     let {title, description, imageUrl,newsUrl} = this.props;
+    return (
+      <div className="my-3">
+        <div className="card">
+          <img src={!imageUrl ? "https://www.hindustantimes.com/india-news/few-feared-trapped-in-j-k-s-ramban-as-under-construction-tunnel-collapses-101653005353560.html" : imageUrl} className="card-img-top" alt="..." />
+          <div className="card-body">
+            <h5 className="card-title">{title}</h5>
+            <p className="card-text">
+             {description}
+            </p>
+            <a href={newsUrl} rel="noreferrer" target="_blank" className="btn btn-sm btn-dark">
+              Read More
+            </a>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default NewsItem;
